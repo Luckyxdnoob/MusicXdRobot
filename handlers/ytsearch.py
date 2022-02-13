204e4e40ca79cbc7fc12864fd3ecd4b9c7674613
Luckyxdnoob <99555719+Luckyxdnoob@users.noreply.github.com>
2022-02-13 07:57:39 +0530

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -1,39 +1,31 @@
-# the logging things
-import logging
-
-from pyrogram.types import Message
-from search_engine_parser import GoogleSearch
+from config import BOT_USERNAME
+from driver.filters import command
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtube_search import YoutubeSearch
 
-from pyrogram import Client as app, filters
 
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logger = logging.getLogger(__name__)
-
-import pyrogram
-
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-@app.on_message(pyrogram.filters.command(["search"]))
+@Client.on_message(command(["search", f"search@{BOT_USERNAME}"]))
 async def ytsearch(_, message: Message):
-    try:
-        if len(message.command) < 2:
-            await message.reply_text("/search needs an argument!")
-            return
-        query = message.text.split(None, 1)[1]
-        m = await message.reply_text("`⏰ Bᴇᴇᴘ... Bᴏᴘ...Sᴇᴀʀᴄʜɪɴɢ...`")
-        results = YoutubeSearch(query, max_results=4).to_dict()
-        i = 0
-        text = ""
-        while i < 4:
-            text += f"Title - {results[i]['title']}\n"
-            text += f"Duration - {results[i]['duration']}\n"
-            text += f"Views - {results[i]['views']}\n"
-            text += f"Channel - {results[i]['channel']}\n"
-            text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
-            i += 1
-        await m.edit(text, disable_web_page_preview=True)
-    except Exception as e:
-        await message.reply_text(str(e))
+    if len(message.command) < 2:
+        return await message.reply_text("/search **needs an argument !**")
+    query = message.text.split(None, 1)[1]
+    m = await message.reply_text("🔎 **Searching...**")
+    results = YoutubeSearch(query, max_results=5).to_dict()
+    text = ""
+    for i in range(5):
+        try:
+            text += f"🏷 **Name:** __{results[i]['title']}__\n"
+            text += f"⏱ **Duration:** `{results[i]['duration']}`\n"
+            text += f"👀 **Views:** `{results[i]['views']}`\n"
+            text += f"📣 **Channel:** {results[i]['channel']}\n"
+            text += f"🔗: https://www.youtube.com{results[i]['url_suffix']}\n\n"
+        except IndexError:
+            break
+    await m.edit_text(
+        text,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🗑 Close", callback_data="cls")]]
+        ),
+    )
