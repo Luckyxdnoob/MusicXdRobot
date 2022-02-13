196934c2fabce3efb90f22bf760e4fbb81e12f82
Luckyxdnoob <99555719+Luckyxdnoob@users.noreply.github.com>
2022-02-13 08:37:12 +0530

    Update chat_member_updated.py

--- handlers/chat_member_updated.py
+++ handlers/chat_member_updated.py
@@ -1,17 +1,38 @@
-from pyrogram import Client
-from pyrogram.types import ChatMemberUpdated
+""" chat database """
 
-from cache import admins as cache
+from typing import Dict, List, Union
+
+from driver.database.dblocal import db
+
+chatsdb = db.chats
 
 
-@Client.on_chat_member_updated()
-async def chat_member_updated(_, chat_member_updated: ChatMemberUpdated):
-    chat = chat_member_updated.chat.id
-    new = chat_member_updated.new_chat_member
+async def get_served_chats() -> list:
+    chats = chatsdb.find({"chat_id": {"$lt": 0}})
+    if not chats:
+        return []
+    chats_list = []
+    for chat in await chats.to_list(length=1000000000):
+        chats_list.append(chat)
+    return chats_list
 
-    if new.can_manage_voice_chats:
-        if new.user.id not in cache.admins[chat]:
-            cache.admins[chat].append(new.user.id)
-    else:
-        if new.user.id in cache.admins[chat]:
-            cache.admins[chat].remove(new.user.id)
+
+async def is_served_chat(chat_id: int) -> bool:
+    chat = await chatsdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return False
+    return True
+
+
+async def add_served_chat(chat_id: int):
+    is_served = await is_served_chat(chat_id)
+    if is_served:
+        return
+    return await chatsdb.insert_one({"chat_id": chat_id})
+
+
+async def remove_served_chat(chat_id: int):
+    is_served = await is_served_chat(chat_id)
+    if not is_served:
+        return
+    return await chatsdb.delete_one({"chat_id": chat_id})
