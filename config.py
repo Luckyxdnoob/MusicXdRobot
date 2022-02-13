141ca873736d61fe88bcff98ca9997979473b0fa
Luckyxdnoob <99555719+Luckyxdnoob@users.noreply.github.com>
2022-02-13 09:00:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ que = {}
 SESSION_NAME = getenv("SESSION_NAME", "BQCiI7KGR4Qrosebn857-QvDqQNoIitfoPzKLOTBEnRu8k4OmjcW0LWA2URSKE556A7X0RAMhlLfGNXWVMRaXtWBAZi9KjmMAVv7Qk4ac0uaJW_j7mKfx1QEPkjBLz9aVfYVp78Xd8sdOe9BB40lz9vrk9FCx_EGfEdLgPQAL0952QeQUQk5KVE24QRyoYMd7PmXZvVWfhho1zSjL5l0V8joWejU6OCN9e9jQfUetcK5PBq4GHEw9LYb-3EaO-R6Lblmk1Y1DcMWih71p5COq9HfYsV1q6SLn7jyP7Nenf3ZrtM70phulYJASs8GatpCLjLmBnqg3vQH023ctMDS7LZge27vYQA")
 BOT_TOKEN = getenv("BOT_TOKEN", "2034558488:AAFCVz6rjcXEu1Opzyfny2oSMIuphk2SUlQ")
 BOT_NAME = getenv("BOT_NAME", "𝙂𝙖𝙡𝙖𝙭𝙞𝙣𝙖 ✘ 𝙈𝙪𝙨𝙞𝙘")
+MONGODB_URL = getenv("MONGODB_URL")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/fb7d30e217de089621c87.png")
 THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/257c2b28860112a84d985.jpg")
 AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/257c2b28860112a84d985.jpg")
